c0dc1682793bd28f99d482cd4dc4e501cfa5baf6
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-09-14 21:16:33 -0300

    Creating MC campaigns

--- src/APIGateway/MailChimpGateway.php
+++ src/APIGateway/MailChimpGateway.php
@@ -7,31 +7,32 @@ use DrewM\MailChimp\MailChimp;
 class MailChimpGateway
 {
     private string $apiKey;
-    private string $listID;
-    private string $segmentID;
+    private string $listId;
+    private string $segmentId;
+    private string $folderId;
+
     private MailChimp $mailChimp;
 
-    public function __construct(string $apiKey, string $listID, string $segmentId)
+    public function __construct(string $apiKey, string $listId, string $segmentId, string $folderId)
     {
         $this->apiKey = $apiKey;
-        $this->listID = $listID;
-        $this->segmentID = $segmentId;
+        $this->listId = $listId;
+        $this->segmentId = $segmentId;
+        $this->folderId = $folderId;
 
         $this->mailChimp = new MailChimp($this->apiKey);
     }
 
     public function send(string $html)
     {
-        echo 'Sending!'.PHP_EOL;
-
         $result = $this->mailChimp->post('campaigns', [
             'type' => 'regular',
             'recipients' => [
                 'segment_opts' => [
-                    'saved_segment_id' => (int)$this->segmentID,
+                    'saved_segment_id' => (int)$this->segmentId,
                     'match' => 'all',
                 ],
-                'list_id' => $this->listID,
+                'list_id' => $this->listId,
             ],
             'settings' => [
                 'subject_line' => 'Ofertas de la semana',
@@ -44,7 +45,10 @@ class MailChimpGateway
         if (array_key_exists('errors', $result)) {
             echo 'Errors found: '.print_r($result['errors'],1).PHP_EOL;
         } else {
-            echo 'Good. '.print_r($result, 1).PHP_EOL;
+            $campaignId = $result['id'];
+            $this->mailChimp->put('campaigns/'.$campaignId, [
+                'html' => $html,
+            ]);
         }
     }
 }
