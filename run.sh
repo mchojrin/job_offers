4c96c496415b88b5e7fd2d1d3490cf519e736ff0
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-09-14 21:17:28 -0300

    Created a run configuration for docker

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run \
+  --network="host" \
+  -v $(pwd):/app \
+  -v $(pwd)/php/conf.d/xdebug.ini:/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \
+  -v $(pwd)/php/conf.d/error_reporting.ini:/usr/local/etc/php/conf.d/error_reporting.ini \
+  -it job_offer app:create-email
